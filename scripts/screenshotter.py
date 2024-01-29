7f7ea3c4d462d3f24a9268bd2d606cda18cad0d1
blacklistedx <marchesejake@outlook.com>
2024-01-29 12:31:37 +1100

    windows keylogger

--- scripts/screenshotter.py
+++ scripts/screenshotter.py
@@ -13,17 +13,17 @@ def get_dimensions():
     return (width, height, left, top)
 
 def screenshot(name='screenshot'):
-    hdesktop = win32gui.GetDesktopWindow()
-    width, height, left, top = get_dimensions()
+    hdesktop = win32gui.GetDesktopWindow()      #Gets a handle on entire screen
+    width, height, left, top = get_dimensions() #Gets screen dimensions
 
-    desktop_dc = win32gui.GetWindowDC(hdesktop)
+    desktop_dc = win32gui.GetWindowDC(hdesktop) #Standard steps when working with screenshots and windows API
     img_dc = win32ui.CreateDCFromHandle(desktop_dc)
-    mem_dc = img_dc.CreateCompatibleDC()
+    mem_dc = img_dc.CreateCompatibleDC()        #
 
-    screenshot = win32ui.CreateBitmap()
-    screenshot.CreateCompatibleBitmap(img_dc, width, height)
-    mem_dc.SelectObject(screenshot)
-    mem_dc.BitBlt((0,0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
+    screenshot = win32ui.CreateBitmap()         #Creates bitmap object to be saved as
+    screenshot.CreateCompatibleBitmap(img_dc, width, height)    #Grabs the object to se memory device to point at bitmap device
+    mem_dc.SelectObject(screenshot)                             #Calls screenshot object
+    mem_dc.BitBlt((0,0), (width, height), img_dc, (left, top), win32con.SRCCOPY)    #Stores in created bitmap memory
     screenshot.SaveBitmapFile(mem_dc, f'{name}.bmp')
 
     mem_dc.DeleteDC()
